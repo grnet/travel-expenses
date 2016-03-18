4a569a2fee286bf846493a81e091d085fd2bdbf1
Kostas Vogias <vogias@grnet.gr>
2016-03-18 11:38:36 +0200

    create unit tests for validators

--- travelsBackend/texpenses/tests.py
+++ travelsBackend/texpenses/tests.py
@@ -1,12 +1,36 @@
-# from django.test import TestCase
+from django.test import TestCase
 from validators import iban_validation
 from validators import afm_validator
-# Create your tests here.
 
-# iban = "GR1601101250000000012300695"
 
-# print iban_validation(iban)
+class UserFormValidation(TestCase):
 
-afm = "123456789"
+    def test_iban_validator(self):
+        """TODO: Docstring for test_iban_validator.
+        :returns: TODO
 
-print afm_validator(afm)
+        """
+        print "\nRunning iban validator test"
+        iban = "GR160110125000000001230069a"
+        iban_validation(iban)
+        print "============================="
+
+    def test_afm_validator(self):
+        """TODO: Docstring for test_afm_validator.
+        :returns: TODO
+
+        """
+        print "\nRunning afm validator test"
+        afm = 1234566789
+        afm_validator(afm)
+        print "============================"
+
+    # def test_user_profile(self):
+        # """TODO: Docstring for test_user_profile.
+        # :returns: TODO
+
+        # """
+        # from django.contrib.auth.models import User
+        # u = User.objects.get(username='admin')
+        # surname = u.get_profile().surname
+        # print surname
