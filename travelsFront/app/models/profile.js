1f6098d5fc3bb86d1d027c686baddfd19550a051
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-04 17:57:28 +0300

    create profile form

--- travelsFront/app/models/profile.js
+++ travelsFront/app/models/profile.js
@@ -1,13 +1,13 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({ 
-	name: DS.attr(),
-	surname: DS.attr(), 
+export default DS.Model.extend({
+	first_name: DS.attr(),
+	last_name: DS.attr(), 
 	iban: DS.attr(),
 	accountID: DS.attr(),
 	specialtyID: DS.attr(),
 	userKind: DS.attr(),
 	taxRegNum: DS.attr(),
 	taxOffice: DS.attr()
-	      
+  
 });
--- travelsFront/tests/unit/models/profile-test.js
+++ travelsFront/tests/unit/models/profile-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('user', 'Unit | Model | user', {
+moduleForModel('profile', 'Unit | Model | profile', {
   // Specify the other units that are required for this test.
   needs: []
 });
