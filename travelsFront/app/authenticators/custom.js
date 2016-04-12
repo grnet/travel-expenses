bc557ca1a5cfaa9c3c8b70212851b2da5ceadcf2
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-12 17:43:08 +0300

    create custom authenticator

---
+++ travelsFront/app/authenticators/custom.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import Base from 'ember-simple-auth/authenticators/base';
+
+export default Base.extend({
+  restore(data) {
+  },
+
+  authenticate(/*args*/) {
+  },
+
+  invalidate(data) {
+  }
+});
