cebb3822d6b7c22ad170bcd1695b9afdc2d141e5
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-14 16:15:24 +0300

    erase test-comments

--- travelsFront/app/controllers/application.js
+++ travelsFront/app/controllers/application.js
@@ -1,14 +1,14 @@
 import Ember from 'ember';
 
-
 export default Ember.Controller.extend({
 	session: Ember.inject.service('session'),
 
 	actions: {
 		invalidateSession() {
+
 			var self=this;
 			var token=self.get("session.data.authenticated.auth_token")
-			console.log(token);
+			
 			Ember.$.ajax({
 				url: "http://127.0.0.1:8000/auth/logout/",
 				method: "POST", 
@@ -17,7 +17,6 @@ export default Ember.Controller.extend({
 					self.get('session').invalidate();
 				},
 				error: function(request,error){
-					//console.log(this.session);
 					alert("Request: "+JSON.stringify(request));
 				}
 			});
