d75cd68e4bcd1efaafbf4489b7b184eafef320bd
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-14 13:10:46 +0300

    implement ajax call

--- travelsFront/app/controllers/application.js
+++ travelsFront/app/controllers/application.js
@@ -1,11 +1,26 @@
 import Ember from 'ember';
 
+
 export default Ember.Controller.extend({
 	session: Ember.inject.service('session'),
 
 	actions: {
+
 		invalidateSession() {
-			this.get('session').invalidate();
-		}
+
+    		Ember.$.ajax({
+    			url: "http://127.0.0.1:8000/auth/logout/",
+    			method: "POST", 
+    			headers: {"Authorization":"Token" + " 30afe8921b6aff3dee130976ed8ca40e3613ae52"}, 
+    			success: function(result){
+    				this.get('session').invalidate();
+    			},
+    			error: function(request,error){
+    				console.log(this.session);
+        			alert("Request: "+JSON.stringify(request));
+    			}
+    		});
+    	}
 	}
+	
 });
