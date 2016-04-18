18f092409346f12dbb47343dc6dd58c2c8d328d2
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-18 15:30:47 +0300

    create petition form

---
+++ travelsFront/app/models/petition.js
@@ -0,0 +1,15 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	travelID: DS.attr(),
+	taskStartDate: DS.attr(),
+	taskEndDate: DS.attr(),
+	departurePoint: DS.attr(),
+	arrivalPoint: DS.attr(),
+	meanOfTransport: DS.attr(),
+	categoryOfTransport: DS.attr(),
+	project: DS.attr(),
+	recMeanOfTransport: DS.attr(),
+ 	recHotel: DS.attr(),
+ 	recCostParticipation: DS.attr()
+});
