716f60b69d9f529407c744881446ca18ff0b4e6f
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-20 11:49:00 +0300

    add reasonoFTransport to petition

--- travelsFront/app/models/petition.js
+++ travelsFront/app/models/petition.js
@@ -3,14 +3,15 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	travelID: DS.attr(),
 	taskStartDate: DS.attr(),
+	taskEndDate: DS.attr(),
 	taskStartTime: DS.attr(),
 	taskEndTime: DS.attr(),
-	taskEndDate: DS.attr(),
 	departurePoint: DS.attr(),
 	arrivalPoint: DS.attr(),
 	meanOfTransport: DS.attr(),
 	categoryOfTransport: DS.attr(),
 	project: DS.attr(),
+	reasonOfTransport: DS.attr(),
 	recMeanOfTransport: DS.attr(),
  	recHotel: DS.attr(),
  	recCostParticipation: DS.attr()
