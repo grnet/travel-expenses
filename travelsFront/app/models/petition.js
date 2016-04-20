fffef980ce833ae2745d4e61ee9d689a69b7b3dc
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-20 11:26:57 +0300

    add time attr to petition model

--- travelsFront/app/models/petition.js
+++ travelsFront/app/models/petition.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	travelID: DS.attr(),
 	taskStartDate: DS.attr(),
+	taskStartTime: DS.attr(),
+	taskEndTime: DS.attr(),
 	taskEndDate: DS.attr(),
 	departurePoint: DS.attr(),
 	arrivalPoint: DS.attr(),
