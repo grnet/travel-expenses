2885476836f4828391a28a0f79b694f5126f51f3
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-21 13:31:13 +0300

    accept petition endpoint

--- travelsFront/app/adapters/application.js
+++ travelsFront/app/adapters/application.js
@@ -14,13 +14,13 @@ export default DS.RESTAdapter.extend(DataAdapterMixin,{
 
 		var url = this._super(modelName, id, snapshot, requestType, query);
 
-		if (modelName === "account" && requestType === "createRecord" ){
+		if (modelName === "account" && requestType === "createRecord"){
 			url = "http://127.0.0.1:8000/auth/register/";
 		}
 		else if (modelName === "profile") {
 			url = "http://127.0.0.1:8000/auth/me/detailed/";
 		}
-		else if (modelName === "petition") {
+		else if (modelName === "petition" && requestType === "createRecord") {
 			url = "http://127.0.0.1:8000/petition/user_petition/";
 		}
 
