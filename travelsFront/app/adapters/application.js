06604887511c2faa524b9ae5faf47cf0f6ca4200
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-21 11:04:44 +0300

    define petition endpoint

--- travelsFront/app/adapters/application.js
+++ travelsFront/app/adapters/application.js
@@ -20,6 +20,9 @@ export default DS.RESTAdapter.extend(DataAdapterMixin,{
 		else if (modelName === "profile") {
 			url = "http://127.0.0.1:8000/auth/me/detailed/";
 		}
+		else if (modelName === "petition") {
+			url = "http://127.0.0.1:8000/petition/user_petition/";
+		}
 
 		return url;
 	},
