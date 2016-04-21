8c29c1259686f97bb02df957f7160be0bdce460b
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-21 13:26:12 +0300

    create adapters for petition lookups

---
+++ travelsFront/app/adapters/departure-point.js
@@ -0,0 +1,26 @@
+import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+
+
+export default DS.RESTAdapter.extend(DataAdapterMixin,{
+	host: 'http://127.0.0.1:8000',
+	namespace: '/petition',
+	contentType: 'application/json',
+	dataType: 'json',
+	authorizer: 'authorizer:token',
+
+
+
+	buildURL: function(modelName, id, snapshot, requestType, query) {
+
+		var url = this._super(modelName, id, snapshot, requestType, query);
+
+		if (modelName === "departure-point"){
+			url = "http://127.0.0.1:8000/petition/departure_point/";
+		}
+
+		return url;
+	} 
+
+});
+
