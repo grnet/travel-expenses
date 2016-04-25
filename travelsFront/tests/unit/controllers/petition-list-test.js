60b7633d7a08364990af0328baabc6d9f1b124da
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-04-25 16:22:54 +0300

    create controller, route & template for petition-list

---
+++ travelsFront/tests/unit/controllers/petition-list-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:petition-list', 'Unit | Controller | petition list', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
