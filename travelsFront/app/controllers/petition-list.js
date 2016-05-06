6701fe6cc96e23a980e91e2fe2c633631a555790
Kostas Vogias <vogias@grnet.gr>
2016-05-06 14:56:45 +0300

    Add petition creation date to template

--- travelsFront/app/controllers/petition-list.js
+++ travelsFront/app/controllers/petition-list.js
@@ -9,18 +9,18 @@ export default Ember.Controller.extend({
 			if (status === "δημιουργηθείσα από μετακινούμενο"){
 
 				self.store.findRecord('petition', id).then(function(petition) {
-	  					petition.destroyRecord().then(function(value) {
-	  						self.set('deleteMessage', "Η αίτηση σας έχει διαγραφεί επιυχώς !")
-	  					}, function(reason) {
-					self.set('deleteMessage', 'Η διαγραφή της αίτησης σας απέτυχε...');
+					petition.destroyRecord().then(function(value) {
+						self.set('deleteMessage', "Η αίτηση σας έχει διαγραφεί επιυχώς !")
+					}, function(reason) {
+						self.set('deleteMessage', 'Η διαγραφή της αίτησης σας απέτυχε...');
 
-				}); 
+					}); 
 				});
 			}
 			else{
 				self.set('deleteMessage', 'Η αίτηση σας έχει υποβληθεί συνεπώς δεν είναι δυνατή η διαγραφή της...');
 			}
-			
+
 		},
 
 		petitionEdit(id){
