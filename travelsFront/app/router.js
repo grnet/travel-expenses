3156fe779f3142376422d714534df4c3afabb952
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-05-30 10:39:59 +0300

    Delete unecessary routes

--- travelsFront/app/router.js
+++ travelsFront/app/router.js
@@ -9,8 +9,6 @@ Router.map(function() {
   this.route('login');
   this.route('signup');
   this.route('profile');
-  this.route('specialty');
-  this.route('kind');
   this.route('petition');
   this.route('petitionList');
   this.route('editPetition', {path: ':petition_id'});
