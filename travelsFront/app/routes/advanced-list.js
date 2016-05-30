7a5e0dbca2ca80cb02b2722f914887b5d65555ae
Kostas Vogias <vogias@grnet.gr>
2016-05-30 12:28:50 +0300

    Add advanced list related files(controller,route etc)

--- travelsFront/app/routes/advanced-list.js
+++ travelsFront/app/routes/advanced-list.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+
+
+export default Ember.Route.extend(AuthenticatedRouteMixin,{
+
+	model() {
+		
+		return this.store.findAll('petition');
+	},
+	setupController: function(controller, model) {
+		this._super(controller, model);
+		controller.set('deleteMessage','');
+	}
 });
