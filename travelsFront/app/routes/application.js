b1550b242761a950f597f7fed420b238aa14a88f
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-06-03 17:32:11 +0300

    Fix syntax error in 'application.js' route

--- travelsFront/app/routes/application.js
+++ travelsFront/app/routes/application.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin,{
-	
+	user: null,
+
 	beforeModel(){
-    	let user = this.get("account").loadCurrentUser());
+    	let user = this.get("account").loadCurrentUser();
 	},	
 });
