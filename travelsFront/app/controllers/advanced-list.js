59d5964304dd5f627c3524a8f4f1beb3c7824b0d
Kostas Vogias <vogias@grnet.gr>
2016-06-14 11:29:34 +0300

    Implement delete and undo on advanced petition listing for secretary users

--- travelsFront/app/controllers/advanced-list.js
+++ travelsFront/app/controllers/advanced-list.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import ENV from 'travels-front/config/environment';
 
 export default Ember.Controller.extend({
 
@@ -11,13 +12,13 @@ export default Ember.Controller.extend({
 
 			var self=this;
 
-			if (status === "δημιουργηθείσα από μετακινούμενο"){
+			if (status === "σε επεξεργασία απο τη γραμματεία" || status === "υποβεβλημένη από μετακινούμενο"){
 
 				self.store.findRecord('petition', id).then(function(petition) {
 
 					petition.destroyRecord().then(function(value) {
 						self.set('statePetitionList', true);
-						self.set('deleteMessage', "Η αίτηση σας με id: " + value.id + " έχει διαγραφεί επιτυχώς !");
+						self.set('deleteMessage', "Η αίτηση με id: " + value.id + " έχει διαγραφεί επιτυχώς !");
 						Ember.$('#divMessage').removeClass('redMessage');
 						Ember.$('#divMessage').addClass('greenMessage');
 						console.log(value.id.name);
@@ -37,13 +38,26 @@ export default Ember.Controller.extend({
 
 			else{
 				self.set('statePetitionList', false);
-				self.set('deleteMessage', 'Η αίτηση σας έχει υποβληθεί συνεπώς δεν είναι δυνατή η διαγραφή της...');
+				self.set('deleteMessage', 'Η αίτηση έχει υποβληθεί συνεπώς δεν είναι δυνατή η διαγραφή της...');
 				Ember.$('#divMessage').removeClass('greenMessage');
 				Ember.$('#divMessage').addClass('redMessage');
 			}
 
 		},
+		petitionUndo(id){
+			var self=this;
+			var model=this.get('model');
 
+
+			var petition=self.store.peekRecord('petition',id);
+
+			self.store.findRecord('petition-status',ENV.petition_status_3).then(function(status){
+				petition.set('status',status);
+				petition.save();
+			});
+
+
+		},
 		petitionEdit(id){
 			id=id.substring(id.indexOf('user_petition/')+14,id.lastIndexOf('/'));
 			this.transitionToRoute('advancedPetition',id);
--- travelsFront/app/validators/afm-validator.js
+++ travelsFront/app/validators/afm-validator.js
@@ -2,20 +2,20 @@ import BaseValidator from 'ember-cp-validations/validators/base';
 
 export default BaseValidator.extend({
 	validate(value/*, options, model, attribute*/) {
-	if (value!=null && value!=''){	
-		var intValue=Number(value);
+		if (value!=null && value!=''){	
+			var intValue=Number(value);
 
-		if (String(intValue)=='NaN') {
-			return "AFM should not contain characters";
-		}
-		var length= value.length;
+			if (String(intValue)=='NaN') {
+				return "AFM should not contain characters";
+			}
+			var length= value.length;
 
-		if(length==null)
-			length=9;
+			if(length==null)
+				length=9;
 
-		if (length!=9) {
-			return 'AFM should be 9 digits, current length:'+length;
-		 }
+			if (length!=9) {
+				return 'AFM should be 9 digits, current length:'+length;
+			}
 		}
 
 		return true;
