3562286676fa7e04750e292d146abfd005cea431
Kostas Vogias <vogias@grnet.gr>
2016-06-24 15:18:38 +0300

    Redirect to rootURL instead of baseURL when logging out

--- travelsFront/app/controllers/application.js
+++ travelsFront/app/controllers/application.js
@@ -4,20 +4,25 @@ import ENV from 'travels-front/config/environment';
 export default Ember.Controller.extend({
 
 	session: Ember.inject.service('session'),
-	
+
 	actions: {
-		
+
 		invalidateSession() {
-			
+
 			var self=this;
 			var token=self.get("session.data.authenticated.auth_token");
-			
+
 			Ember.$.ajax({
 				url: ENV.APP.backend_host+"/auth/logout/",
 				method: "POST", 
 				headers: {"Authorization":"Token " + token}, 
 				success: function(result){
-					self.get('session').invalidate();
+					self.get('session').invalidate().then(function(){
+						if (!Ember.testing) {
+
+							window.location.replace(ENV.APP.app_url_prefix);
+						}
+					});
 				},
 				error: function(request,error){
 					alert("Request: " + JSON.stringify(request));
