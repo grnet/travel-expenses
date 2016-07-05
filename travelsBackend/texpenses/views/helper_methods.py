dca97a7378142af80dcef654751bf95f028ebec3
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-07-05 14:34:27 +0300

    add function to model package

--- travelsBackend/texpenses/views/helper_methods.py
+++ travelsBackend/texpenses/views/helper_methods.py
@@ -1,23 +1,6 @@
 import datetime
 
 
-def get_queryset_on_group(request_user, model):
-    """TODO: Docstring for get_queryset_on_group.
-
-    :user: TODO
-    :returns: TODO
-
-    """
-    user_groups = request_user.groups.all()
-
-    if user_groups:
-        user_group_name = user_groups[0].name
-
-    if request_user.is_staff or user_group_name == "SECRETARY":
-
-        return model.objects.all()
-    else:
-        return model.objects.filter(user=request_user)
 
 
 def checkPetitionCompleteness(request, status):
