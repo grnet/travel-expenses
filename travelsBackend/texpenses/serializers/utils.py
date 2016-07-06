50c300af9352bd286181c3f3f4f0aafeff4fac02
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-07-06 10:46:37 +0300

    Simplify the loading of a module based on its name

--- travelsBackend/texpenses/serializers/utils.py
+++ travelsBackend/texpenses/serializers/utils.py
@@ -1,23 +1,29 @@
+import importlib
 import pkgutil
 import re
 
+
 CUSTOM_SERIALIZER_CODE = 'texpenses.serializers'
-exec("import " + CUSTOM_SERIALIZER_CODE + " as custom_serializer_code")
 
 
-def get_package_modules():
-    """TODO: Docstring for get_package_files.
-    :returns: TODO
-
+def get_package_module(module_name):
     """
-    package = custom_serializer_code
-    modules = []
-    for importer, modname, ispkg in pkgutil.iter_modules(package.__path__):
-        if not ispkg:
-            modules += [modname, ]
-    return modules
+    This function loads programtically the desired module which is located in
+    the default package. In case, it can't find such a module, it returns
+    `None`.
+
+    :param module_name: Name of module inside the package.
+
+    :returns: The module object is it exists; `None` otherwise.
+    """
+    try:
+        return importlib.import_module(
+            CUSTOM_SERIALIZER_CODE + '.' + module_name)
+    except ImportError:
+        return None
 
 
-def camel2snake(name):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+def camel2snake(name_camel_case):
+    """ Converts a CamelCase string to snake_case. """
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name_camel_case)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
