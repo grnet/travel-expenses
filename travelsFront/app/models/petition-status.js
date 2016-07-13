886a62b570e688d8c55a6e948f86c2fc75cd941c
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-07-13 14:40:52 +0300

    Add a computed  property to get the status ID

--- travelsFront/app/models/petition-status.js
+++ travelsFront/app/models/petition-status.js
@@ -2,6 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	name: DS.attr(),
-	url: DS.attr()
+	url: DS.attr(),
+	statusID: Ember.computed('id', function(){
+  	// return just the status id
+    return _.last(this.get('id').replace(/\/$/, '').split('/'));
+  })
   
 });
