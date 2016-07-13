d46b3bce363f3a24e449d5a5b518f21f6cba9c1e
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-07-13 14:45:30 +0300

    Create a component for petition list

---
+++ travelsFront/app/components/petition-listing.js
@@ -0,0 +1,70 @@
+import Ember from 'ember';
+import ENV from 'travels-front/config/environment'; 
+
+export default Ember.Component.extend({
+
+	deleteMessage: "",
+	statePetitionList: "",
+
+	actions: {
+
+		petitionDelete(id,status){
+
+			var self=this;
+			var model=this.get('model');
+
+			if (status === "δημιουργηθείσα από μετακινούμενο"){
+
+				self.store.findRecord('petition', id).then(function(petition) {
+					petition.destroyRecord().then(function() {
+
+						self.set('statePetitionList', true);
+						self.set('deleteMessage', "Η αίτηση σας έχει διαγραφεί επιτυχώς !");
+						Ember.$('#messageModal').modal();
+						Ember.$('#styleModal').removeClass('btn-warning');
+						Ember.$('#styleModal').addClass('btn-success');
+						
+
+					}, function(reason) {
+						self.set('statePetitionList', false);
+						self.set('deleteMessage', 'Η διαγραφή της αίτησης σας απέτυχε...');
+						Ember.$('#messageModal').modal();
+						Ember.$('#styleModal').removeClass('btn-success');
+						Ember.$('#styleModal').addClass('btn-warning');
+
+					}); 
+
+				});	
+
+			}
+
+			else{
+				self.set('statePetitionList', false);
+				self.set('deleteMessage', 'Η αίτηση σας έχει υποβληθεί συνεπώς δεν είναι δυνατή η διαγραφή της...');
+				Ember.$('#messageModal').modal();
+				Ember.$('#styleModal').removeClass('btn-success');
+				Ember.$('#styleModal').addClass('btn-warning');
+			}
+
+		},
+		petitionUndo(id){
+			var self=this;
+			var model=this.get('model');
+
+
+			var petition=self.store.peekRecord('petition',id);
+
+			self.store.findRecord('petition-status',ENV.petition_status_1).then(function(status){
+				petition.set('status',status);
+				petition.save();
+			});
+
+
+		},
+
+		petitionEdit(id){
+			id=id.substring(id.indexOf('user_petition/')+14,id.lastIndexOf('/'));
+			this.transitionToRoute('petition', id);
+		},	
+	}
+});
