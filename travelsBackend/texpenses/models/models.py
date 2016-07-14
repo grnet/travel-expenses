5b9cc3c066fd645857dbbc766468f892c2c8107e
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-07-14 15:13:36 +0300

    Move all model classes in one file; name it `models.py`

--- travelsBackend/texpenses/models/models.py
+++ travelsBackend/texpenses/models/models.py
@@ -1,14 +1,68 @@
+from django.conf import settings
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.utils import timezone
 from django.core.validators import MinValueValidator
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import Sum
 from workdays import networkdays
-from texpenses.models.user_related import UserProfile, TaxOffice
 from texpenses.models.services import get_queryset_on_group
 from texpenses.models import common
 from texpenses.validators import (
     afm_validator, iban_validation, required_validator, date_validator)
+
+
+class TaxOffice(models.Model):
+
+    """Docstring for TaxOffice. """
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    kindDescription = models.CharField(max_length=300, blank=True)
+    address = models.CharField(max_length=20)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=20)
+
+    class APITravel(object):
+        fields = ('name', 'kindDescription', 'address',
+                  'email', 'phone', 'id', 'url',)
+
+    def __unicode__(self):
+        return self.name
+
+
+class UserProfile(AbstractUser):
+    SPECIALTIES = tuple([(k, v) for k, v in common.SPECIALTY.iteritems()])
+    KINDS = tuple([(k, v) for k, v in common.KIND.iteritems()])
+    USER_CATEGORIES = tuple([(category, category)
+                             for category in common.USER_CATEGORIES])
+    iban = models.CharField(max_length=200, blank=True, null=True,
+                            validators=[iban_validation])
+    specialtyID = models.CharField(max_length=10, choices=SPECIALTIES)
+    taxRegNum = models.CharField(max_length=9, blank=True, null=True,
+                                 validators=[afm_validator])
+    taxOffice = models.ForeignKey(TaxOffice, blank=True, null=True)
+    kind = models.CharField(max_length=10, choices=KINDS, blank=True, null=True)
+    category = models.CharField(max_length=1, choices=USER_CATEGORIES,
+                                blank=False, null=False, default='B')
+    trip_days_left = models.IntegerField(default=settings.MAX_HOLIDAY_DAYS)
+
+    class APITravel(object):
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'password',
+                  'iban', 'specialtyID', 'kind', 'taxRegNum', 'taxOffice',
+                  'category', 'user_group', 'trip_days_left')
+        read_only_fields = (
+            'username',
+            'password',
+            'trip_days_left'
+        )
+
+    def user_group(self):
+        groups = self.groups.all()
+        print groups
+        if not groups:
+            return "Unknown"
+        return groups[0].name
 
 
 class Project(models.Model):
--- travelsBackend/texpenses/models/user_related.py
+++ travelsBackend/texpenses/models/user_related.py
@@ -1,58 +1,7 @@
 from django.db import models
-from django.conf import settings
+from djago.conf import settings
 from django.contrib.auth.models import AbstractUser
 from texpenses.validators import afm_validator, iban_validation
 from texpenses.models import common
 
 
-class TaxOffice(models.Model):
-
-    """Docstring for TaxOffice. """
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    kindDescription = models.CharField(max_length=300, blank=True)
-    address = models.CharField(max_length=20)
-    email = models.EmailField(blank=True)
-    phone = models.CharField(max_length=20)
-
-    class APITravel(object):
-        fields = ('name', 'kindDescription', 'address',
-                  'email', 'phone', 'id', 'url',)
-
-    def __unicode__(self):
-        return self.name
-
-
-class UserProfile(AbstractUser):
-    SPECIALTIES = tuple([(k, v) for k, v in common.SPECIALTY.iteritems()])
-    KINDS = tuple([(k, v) for k, v in common.KIND.iteritems()])
-    USER_CATEGORIES = tuple([(category, category)
-                             for category in common.USER_CATEGORIES])
-    iban = models.CharField(max_length=200, blank=True, null=True,
-                            validators=[iban_validation])
-    specialtyID = models.CharField(max_length=10, choices=SPECIALTIES)
-    taxRegNum = models.CharField(max_length=9, blank=True, null=True,
-                                 validators=[afm_validator])
-    taxOffice = models.ForeignKey(TaxOffice, blank=True, null=True)
-    kind = models.CharField(max_length=10, choices=KINDS, blank=True, null=True)
-    category = models.CharField(max_length=1, choices=USER_CATEGORIES,
-                                blank=False, null=False, default='B')
-    trip_days_left = models.IntegerField(default=settings.MAX_HOLIDAY_DAYS)
-
-    class APITravel(object):
-        fields = ('username', 'first_name', 'last_name',
-                  'email', 'password',
-                  'iban', 'specialtyID', 'kind', 'taxRegNum', 'taxOffice',
-                  'category', 'user_group', 'trip_days_left')
-        read_only_fields = (
-            'username',
-            'password',
-            'trip_days_left'
-        )
-
-    def user_group(self):
-        groups = self.groups.all()
-        print groups
-        if not groups:
-            return "Unknown"
-        return groups[0].name
