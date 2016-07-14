1006fc3b224aedd40f9d7708430afb250e030637
Thodoris Sotiropoulos <theosotr@windowslive.com>
2016-07-14 11:41:21 +0300

    Remove useless entities

--- travelsBackend/texpenses/admin.py
+++ travelsBackend/texpenses/admin.py
@@ -1,46 +1,24 @@
 from django.contrib import admin
 from .models import UserProfile
-from .models import Specialty
 from .models import TaxOffice
-from .models import Kind
-from .models import MovementCategories
 from .models import Petition
 from .models import Project
-from .models import Transportation
-from .models import PetitionStatus
-from .models import CountryCategory
 from .models import Country
 from .models import City
-from .models import UserCategory
 
 from .models import AdditionalExpenses
-from .models import Compensation
-from .models import FeedingKind
-from .models import Flight
-from .models import Accomondation
 # Register custom models here.
 
 # user profile related models
 admin.site.register(UserProfile)
-admin.site.register(Specialty)
 admin.site.register(TaxOffice)
-admin.site.register(Kind)
-admin.site.register(UserCategory)
 
 # user petition related models
-admin.site.register(MovementCategories)
 admin.site.register(Petition)
 admin.site.register(Project)
-admin.site.register(Transportation)
-admin.site.register(PetitionStatus)
 
 # advanced petition info models
 admin.site.register(AdditionalExpenses)
-admin.site.register(Compensation)
-admin.site.register(FeedingKind)
-admin.site.register(Flight)
-admin.site.register(Accomondation)
 
-admin.site.register(CountryCategory)
 admin.site.register(Country)
 admin.site.register(City)
