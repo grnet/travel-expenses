68d29df418a8598405217530c27ece446f497e78
Thodoris Sotiropoulos <theosotr@windowslive.com>
2016-07-14 11:51:06 +0300

    trivial fixes

--- travelsBackend/texpenses/serializers/user_related.py
+++ travelsBackend/texpenses/serializers/user_related.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from djoser import settings, serializers as djoser_serializers
 from django.contrib.auth.models import Group
-from texpenses.models import UserCategory
 
 User = get_user_model()
 
@@ -14,10 +13,8 @@ class CustomUserRegistrationSerializer(
         group, created = Group.objects.get_or_create(name='USER')
         group.user_set.add(user)
 
-        category, created = UserCategory.objects.get_or_create(name='B')
 
         if settings.get('SEND_ACTIVATION_EMAIL'):
             user.is_active = False
-            user.category = category
             user.save(update_fields=['is_active', 'category'])
         return user
