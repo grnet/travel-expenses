7af65337c5fef4d7be33f3df78643d50f325e2c7
Thodoris Sotiropoulos <theosotr@windowslive.com>
2016-07-14 11:48:23 +0300

    Add module for custom creation, update and validation of petitions

---
+++ travelsBackend/texpenses/serializers/petition.py
@@ -0,0 +1,43 @@
+from texpenses.models import TravelInfo
+
+
+def create(self, validated_data):
+    travel_info = validated_data.pop('travel_info', [])
+    petition = self.Meta.model.objects.create(**validated_data)
+    for travel in travel_info:
+        travel_obj = TravelInfo(travel_petition=petition, **travel)
+        travel_obj.save()
+        petition.travel_info.add(travel_obj)
+    return petition
+
+
+def update(self, instance, validated_data):
+    travel_info = validated_data.pop('travel_info', [])
+    current_travel_info = instance.travel_info.all()
+    for k, v in validated_data.iteritems():
+        setattr(instance, k, v)
+    instance.save()
+    for i, travel in enumerate(travel_info):
+        if i < len(current_travel_info) and current_travel_info:
+            current_travel_obj = current_travel_info[i]
+            for k, v in travel.iteritems():
+                setattr(current_travel_obj, k, v)
+            current_travel_obj.save()
+        else:
+            travel_obj = TravelInfo(travel_petition=instance, **travel)
+            instance.travel_info.create(travel_petition=instance, **travel)
+            travel_obj.save()
+    return instance
+
+
+def validate(self, attrs):
+    model = self.Meta.model
+    attrs = super(self.__class__, self).validate(attrs)
+    nested_attrs = attrs.pop('travel_info', [])
+    for nested in nested_attrs:
+        nested_inst = TravelInfo(travel_petition=model(**attrs), **nested)
+        nested_inst.clean()
+    model_inst = model(**attrs)
+    model_inst.clean()
+    attrs['travel_info'] = nested_attrs
+    return attrs
