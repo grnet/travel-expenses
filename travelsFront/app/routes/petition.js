2612ece936aafafc8b134aa17954dd7168ff0dcb
Kostas Papadimitriou <kpap@grnet.gr>
2016-07-19 18:28:27 +0300

    Clean up petition route

--- travelsFront/app/routes/petition.js
+++ travelsFront/app/routes/petition.js
@@ -2,90 +2,61 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import ENV from 'travels-front/config/environment'; 
 
+const { get } = Ember;
+
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
+
   newPetition: true,
+  modelName: 'user-petition',
+
 	model(params) {
     let petitionId = params.petition_id;
     let model;
     if (petitionId && petitionId != "new") {
       // edit petition
-      model = this.store.findRecord('petition', petitionId);
+      model = this.store.findRecord(this.modelName, petitionId);
       this.set('newPetition', false);
     } else {
       // create petition
-      model = this.store.createRecord('petition');
-      this.store.findAll("petition-status").then(function() {
-        model.set("status", this.store.peekRecord('petition-status', ENV.petition_status_1));
-      }.bind(this));
+      model = this.store.createRecord(this.modelName);
       this.set('newPetition', true);
     }
-		return model
-	},
-
-	afterModel(petition){
-
-    if (!this.get("newPetition")) { return; }
     
-    // default profile values for new records
-		this.store.findRecord('profile', 1).then(function(profile) {
-			let pet_name=profile.get('first_name');	
-			let pet_surname=profile.get('last_name');
-			let pet_iban=profile.get('iban');
-			let pet_specialty=profile.get('specialtyID');
-			let pet_kind=profile.get('kind');
-			let pet_taxNum=profile.get('taxRegNum');
-			let pet_taxOffice=profile.get('taxOffice');
-			let pet_category=profile.get('category');
-
-			petition.set('name', pet_name);
-			petition.set('surname',pet_surname);
-			petition.set('iban', pet_iban);
-			petition.set('specialtyID', pet_specialty);
-			petition.set('kind', pet_kind);
-			petition.set('taxRegNum', pet_taxNum);
-			petition.set('taxOffice', pet_taxOffice);
-			petition.set('user_category', pet_category);
-		});
-
+		return new Ember.RSVP.Promise(function(resolve, reject) {
+      // resolve user profile
+      this.store.findRecord('profile', 1).then(function(profile) {
+        // if petition is new initialize it with user profile info
+        if (this.get('newPetition')) {
+          this.initializePetition(model, profile)
+          resolve(model);
+        } else {
+          // delegate stuff to model promise
+          return model.then(resolve).catch(reject);
+        }
+      }.bind(this));
+    }.bind(this));
 	},
 
-	setupController: function(controller, model) {
-		this._super(controller, model);
+  // copy common profile fields to petition object
+	initializePetition(petition, profile) {
+
+    let commonFields = [
+      'first_name',
+      'last_name',
+      'iban',
+      'specialty',
+      'kind',
+      'tax_reg_num',
+      'tax_office',
+      'category'
+    ];
+
+    for (let field of commonFields) {
+      petition.set(field, get(profile, field));
+    }
 	},
 
 	actions: {
-		willTransition(transition) {
-			
-      		if (this.controller.get('petitionNotSaved') &&
-          		!confirm('Are you sure you want to abandon progress?Any changes will be lost unless you save them')) {
-
-        		transition.abort();
-     		} 
-     		else {
-        		// Bubble the `willTransition` action so that
-        		// parent routes can decide whether or not to abort.
-        		return true;
-      		}
-    	},
-
-		// willTransition with custom modal	
-		// willTransition(transition) {
-			
-		// 	if (this.controller.get('petitionNotSaved')) {
-		// 		console.log("petitionNotSaved", this.controller.get('petitionNotSaved'));
-				
-		// 		transition.abort();
-
-		// 		Ember.$('#confirmModal').modal();
-		// 		Ember.$('#leave').on('click', () => {
-		// 			console.log("Leave is clicked");
-  		//   		this.transitionTo(transition.handlerInfos[1].name);
-		// 		});
-		// 	}
-		// 	else {
-		// 		return true;
-		// 	}				
-		// },
 	}
 	
 });
