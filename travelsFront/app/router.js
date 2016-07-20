d76a84211ec2ee09fc7e1966cdfba6aced2ea1d7
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-07-20 13:42:49 +0300

    Add route and controller for user-petition

--- travelsFront/app/router.js
+++ travelsFront/app/router.js
@@ -3,17 +3,18 @@ import config from './config/environment';
 import ENV from 'travels-front/config/environment'; 
 
 const Router = Ember.Router.extend({
-	location: config.locationType,
+    location: config.locationType,
 });
 
 Router.map(function() {
-	this.route('login');
-	this.route('signup');
-	this.route('profile');
-	this.route('petition', {path: 'petition/:petition_id'});
-	this.route('petitionList');
-	this.route('advancedPetition',{path:':ap_id'});
-	this.route('advancedList');
+  this.route('login');
+  this.route('signup');
+  this.route('profile');
+  // this.route('petition', {path: 'petition/:petition_id'});
+  // this.route('petitionList');
+  // this.route('advancedPetition',{path:':ap_id'});
+  // this.route('advancedList');
+  this.route('userPetition', {path: 'userPetition/:petition_id'});
 });
 
 export default Router;
