03fb9ffba8cf0a259bcf5397bdf79c7c7be603bb
Olga Brani <olgabrani@grnet.gr>
2016-07-21 14:39:54 +0300

    Add link to h1 and redirect homepage to projectList

--- travelsFront/app/router.js
+++ travelsFront/app/router.js
@@ -10,10 +10,7 @@ Router.map(function() {
   this.route('login');
   this.route('signup');
   this.route('profile');
-  // this.route('petition', {path: 'petition/:petition_id'});
   this.route('petitionList');
-  // this.route('advancedPetition',{path:':ap_id'});
-  // this.route('advancedList');
   this.route('userPetition', {path: 'userPetition/:petition_id'});
 });
 
