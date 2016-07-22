b88428cfa3066a9c50776e198eb035c9dc4c5506
Kostas Papadimitriou <kpap@grnet.gr>
2016-07-22 17:06:13 +0300

    Update datetime component

--- travelsFront/app/components/model-form/fields/date.js
+++ travelsFront/app/components/model-form/fields/date.js
@@ -6,25 +6,12 @@ const { get, computed, computed: { alias } } = Ember;
 
 export default PaperInput.extend(BaseField, {
   tagName: 'md-input-container',
-  dateValue: computed('value', function() {
-    return get(this, 'value') || null;
-  }),
 
-  didInsertElement() {
-    this._super();
-    let input = this.$().find("input");
-    input.addClass("md-input");
-    let cont = this.$().find(".input-group")
-    let icon = this.$().find("md-icon");
-    cont.find("span").append(icon).css({
-      position: 'absolute', 
-      top: 0, 
-      right: 15
-    });
-    cont.css({position: 'relative'});
-  },
+  dateValue: alias('value'),
 
   handleDate(val) {
     this.sendAction('onChange', val);
-  }
+  },
+
+  setValue() {}
 });
