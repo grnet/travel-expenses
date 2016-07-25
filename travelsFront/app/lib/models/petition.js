c11adf7d3211dc3fa13de81a1e16f56a76b4fdc5
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-07-25 12:23:09 +0300

    Add all required fields in user's petition form

--- travelsFront/app/lib/models/petition.js
+++ travelsFront/app/lib/models/petition.js
@@ -8,27 +8,29 @@ export var Petition = DS.Model.extend({
   first_name: DS.attr({attrs: {readonly: true, required: true}}),
   last_name: DS.attr({attrs: {readonly: true, required: true}}),
   specialty: DS.attr({'label': 'Specialty', 'choices': CHOICES.SPECIALTY, 'required': true}),
-  kind: DS.attr({'choices': CHOICES.KIND}),
-  tax_reg_num: DS.attr({'label': 'VAT'}),
-  tax_office: DS.belongsTo('tax-office'),
-  iban: DS.attr(),
-  user_category: DS.attr({'label': 'User Category', 'choices': CHOICES.USER_CATEGORY, 'required': true}),
+  kind: DS.attr({'choices': CHOICES.KIND, 'required': true}),
+  tax_reg_num: DS.attr({attrs: {required: true}, label: 'VAT'}),
+  tax_office: DS.belongsTo('tax-office', {attrs: {required: true}}),
+  iban: DS.attr({attrs: {required: true}}),
+  user_category: DS.attr({'label': 'User Category', 'choices': CHOICES.USER_CATEGORY, 'readonly': true}),
   user: DS.belongsTo('profile'),
   dse: DS.attr('string', {attrs: {required: true}}),
   project: DS.belongsTo('project', {attrs: {required: true}}),
-  reason: DS.attr(),
+  reason: DS.attr({attrs: {required: true}}),
   movement_category: DS.attr({attrs: {readonly: true}}),
-  departure_point: DS.belongsTo('city'),
-  arrival_point: DS.belongsTo('city'),
+  departure_point: DS.belongsTo('city', {attrs: {required: true}}),
+  arrival_point: DS.belongsTo('city', {attrs: {required: true}}),
   task_start_date: DS.attr({
     attrs: {
-      type: 'datetime-local'
+      type: 'datetime-local',
+      required: true
     },
     label: 'Task starts at'
   }),
   task_end_date: DS.attr({
     attrs: {
-      type: 'datetime-local'
+      type: 'datetime-local',
+      required: true
     },
     label: 'Task ends at'
   }),
