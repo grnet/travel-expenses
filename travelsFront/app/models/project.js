fe1f301d4a642a27af6ceb7ca47fbb85bd7ab294
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-07-27 13:45:23 +0300

    Show manager's name in project select tag

--- travelsFront/app/models/project.js
+++ travelsFront/app/models/project.js
@@ -4,6 +4,9 @@ export default DS.Model.extend({
   __api__: {
     ns: 'resources'
   },
+  full_label: Ember.computed('name', function(){
+    return this.get('name') + " (" + this.get('manager_name') + " " + this.get('manager_surname') + ")"
+  }),
 	name: DS.attr(),
 	accountingCode: DS.attr(),
 	manager_name: DS.attr(),
