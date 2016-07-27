458f3b44c1455a76ee4e545e72610305e2245d46
Kostas Papadimitriou <kpap@grnet.gr>
2016-07-27 12:47:20 +0300

    Update profile model config

--- travelsFront/app/models/profile.js
+++ travelsFront/app/models/profile.js
@@ -8,23 +8,41 @@ const CHOICES = ENV.APP.resource_choices;
 
 var Validations=buildValidations({
   email: [
-    validator('presence', true),
+    validator('presence', {presence: true}),
     validator('format', { type: 'email' })
   ],
   iban: validator('iban-validator'),
   first_name: [
-    validator('presence', true),
-    validator('length', {min: 1})
+    validator('presence', {presence: true}),
+    validator('length', {min: 2})
   ],
   last_name: [
     validator('presence', true),
-    validator('length', {min: 1})
+    validator('length', {min: 2})
   ],
   taxRegNum: [
+    validator('presence', {presence: true}),
     validator('length', {min: 1}),
     validator('afm-validator')
   ]
 });
+
+const UI_DEFAULT = {
+  fieldsets: [
+    {
+      'label': 'My account',
+      'fields': ['username', 'email', 'first_name', 'last_name']
+    },
+    {
+      'label': 'Personal info',
+      'fields': ['specialty', 'kind', 'tax_reg_num', 'tax_office', 'iban', 'category']
+    }
+  ],
+  layout: {
+    flex: [50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
+  }
+};
+
 
 export default DS.Model.extend(Validations, {
   __api__: {
@@ -37,33 +55,40 @@ export default DS.Model.extend(Validations, {
   },
 
   __ui__: {
-    'default': {
-      fieldsets: [
-        {
-          'label': 'My account',
-          'fields': ['username', 'email']
-        },
-        {
-          'label': 'Personal info',
-          'fields': ['first_name', 'last_name', 'specialty', 'kind', 'tax_reg_num', 'tax_office', 'iban', 'category']
-        }
-      ],
-      layout: {
-        flex: [50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
-      }
-    }
+    'default': UI_DEFAULT
   },
   
-
-	'username': DS.attr({attrs: {readonly: true}}),
-	'email': DS.attr({hint: 'A valid email address'}),
+	'username': DS.attr({
+    attrs: {
+      disabled: true
+    }
+  }),
+	'email': DS.attr({
+    hint: 'A valid email address'
+  }),
 	'first_name': DS.attr(),
 	'last_name': DS.attr(), 
-	'iban': DS.attr({'label': 'IBAN'}),
-	'specialty': DS.attr({'label': 'Specialty', 'choices': CHOICES.SPECIALTY}),
-	'kind': DS.attr({'choices': CHOICES.KIND}),
-	'tax_reg_num': DS.attr({'label': 'VAT'}),
-	'tax_office': DS.belongsTo('tax-office'),
-	'category': DS.attr({'label': 'User Category', 'choices': CHOICES.USER_CATEGORY}),
+	'iban': DS.attr({
+    'label': 'IBAN'
+  }),
+	'specialty': DS.attr({
+    'label': 'Specialty',
+    'choices': CHOICES.SPECIALTY
+  }),
+	'kind': DS.attr({
+    'choices': CHOICES.KIND
+  }),
+	'tax_reg_num': DS.attr({
+    'label': 'VAT'
+  }),
+	'tax_office': DS.belongsTo('tax-office', {
+    attrs:{
+      autocomplete: true,
+    }
+  }),
+	'category': DS.attr({
+    'label': 'User Category', 
+    'choices': CHOICES.USER_CATEGORY
+  }),
   'user_group': DS.attr(),
 });
