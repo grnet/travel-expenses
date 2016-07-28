07fe8a4fd6d20d308ec8f6afd7e2e1e96bbc4ce8
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-07-28 12:25:03 +0300

    Add travel_info complete list of fields

--- travelsFront/app/models/travel-info.js
+++ travelsFront/app/models/travel-info.js
@@ -1,24 +1,42 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  __api__: {
+    ns: 'petition'
+  },
   url: DS.attr(),
-	depart_date: DS.attr({
-    attrs: {
-      type: 'datetime-local'
-    },
-  }),
-	return_date: DS.attr({
-    attrs: {
-      type: 'datetime-local'
-    },
-  }),
-  departure_point: DS.belongsTo('city'),
-  arrival_point: DS.belongsTo('city'),
-  transportation: DS.attr(),
-  accomondation_price: DS.attr(),
-  transportation_price: DS.attr(),
-  transport_days_manual: DS.attr(),
-  overnights_num_manual: DS.attr(),
-  meals: DS.attr(),
-  movement_num: DS.attr()
+  departure_point: DS.belongsTo('city', {attrs: {required: true}}),
+  arrival_point: DS.belongsTo('city', {attrs: {required: true}}),
+  // depart_date: DS.attr('date', {
+  //   attrs: {
+  //     time: true
+  //   },
+  //   label: 'Depart at'
+  // }),
+  // return_date: DS.attr('date', {
+  //   attrs: {
+  //     time: true
+  //   },
+  //   label: 'Return at'
+  // }),
+  // means_of_transport: DS.attr({'label': 'Means of Transport', 'choices': CHOICES.TRANSPORTATION}),
+  // accomondation_price: DS.attr(),
+  // transportation_price: DS.attr(),
+  // transport_days_manual: DS.attr(),
+  // overnights_num_manual: DS.attr(),
+  // meal: DS.attr({'label': 'Meals', 'choices': CHOICES.MEALS}),
+  // accommodation_default_currency: DS.attr(),
+  // accommodation_local_price: DS.attr(),
+  // accommodation_local_currency: DS.attr(),
+  // accommodation_payment_way: DS.attr(),
+  // accommodation_payment_description: DS.attr(),
+  // transportation_default_currency: DS.attr(),
+  // transportation_payment_way: DS.attr(),
+  // transportation_payment_description: DS.attr(),
+  // transport_days_proposed: DS.attr(),
+  // overnight_cost: DS.attr(),
+  // compensation_level: DS.attr(),
+  // same_day_return_task: DS.attr(),
+  // get_compensation: DS.attr(),
+  // compensation_days_manual: DS.attr()
 });
