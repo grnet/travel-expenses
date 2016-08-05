2142ac9d54ae7ed16b7213320b18be0a393fa89e
Kostas Vogias <vogias@grnet.gr>
2016-08-05 14:18:42 +0300

    Remove blank line

--- travelsBackend/texpenses/permissions/custom_permissions.py
+++ travelsBackend/texpenses/permissions/custom_permissions.py
@@ -12,5 +12,4 @@ class IsOwnerOrAdmin(permissions.BasePermission):
         return request.user or request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
-
         return obj.user == request.user or request.user.is_staff
