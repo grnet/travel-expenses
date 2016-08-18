f46055cfd2a1434476ce5dbe95072cfc53bde32c
Olga Brani <olgabrani@grnet.gr>
2016-08-18 16:33:53 +0300

    Determine local currency list on petition init

--- travelsFront/app/components/petition-currency.js
+++ travelsFront/app/components/petition-currency.js
@@ -15,12 +15,12 @@ const {
 export default Select.extend({
   layoutName: 'components/model-form/fields/select',
 
-  observechoice: observer('object.arrival_point', function() {
+  observechoice: Ember.on('init', observer('object.arrival_point', function() {
     var local = this.get('object.arrival_point.country.currency');
     if (!(_.isUndefined(local)) && (local!= ENV.default_currency)  ) {
       this.get('field').set('choices', [[ENV.default_currency, ENV.default_currency], [local, local]]);
     } else {
       this.get('field').set('choices', [[ENV.default_currency, ENV.default_currency]]);
     }
-  })
+  }))
 });
