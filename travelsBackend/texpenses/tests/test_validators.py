8a09d6c4f7df60d96f4a6c881c2e3714b4413a9b
Kostas Vogias <vogias@grnet.gr>
2016-08-23 14:26:58 +0300

    Test iban validator

--- travelsBackend/texpenses/tests/test_validators.py
+++ travelsBackend/texpenses/tests/test_validators.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from texpenses.validators import date_validator, start_end_date_validator
+from texpenses.validators import date_validator, start_end_date_validator,\
+    iban_validation
 
 
 class ValidatorTest(TestCase):
+
     def test_date_validaror(self):
         date = datetime.now()
         self.assertRaises(ValidationError, date_validator, date)
@@ -22,3 +24,20 @@ class ValidatorTest(TestCase):
         end -= timedelta(days=1)
         self.assertRaises(ValidationError, start_end_date_validator,
                           ((start, end),), (('', ''),))
+
+    def test_iban_validator(self):
+        iban = 'GR4902603280000910200635494'
+
+        try:
+            iban_validation(iban)
+        except ValidationError:
+            self.fail("iban_validation raised ValidationError!")
+
+        iban = 'GR4902603280000910200'
+        self.assertRaises(ValidationError, iban_validation, iban)
+
+        iban = 'G4902603280000910200635494'
+        self.assertRaises(ValidationError, iban_validation, iban)
+
+        iban = ''
+        self.assertRaises(ValidationError, iban_validation, iban)
