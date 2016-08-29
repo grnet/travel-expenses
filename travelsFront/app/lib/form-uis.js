25f0e1c03b5f4cfd0ed65c04db85bfc66666c4e0
Kostas Papadimitriou <kpap@grnet.gr>
2016-08-29 13:03:18 +0300

    Update translations

--- travelsFront/app/lib/form-uis.js
+++ travelsFront/app/lib/form-uis.js
@@ -2,11 +2,11 @@ export const UIS = {
   "petition_user": {
     fieldsets: [
       {
-        'label': 'User data',
+        'label': 'user_data.label',
         'fields': ['first_name', 'last_name', 'specialty', 'kind', 'tax_reg_num', 'tax_office', 'iban', 'user_category']
       },
       {
-        'label': 'Travel Data',
+        'label': 'travel_data.label',
         'fields': ['dse', 'project', 'reason', 'departure_point', 'arrival_point','movement_category', 'country_category',
         'task_start_date', 'task_end_date', 'depart_date', 'return_date', 'means_of_transport', 'transportation_cost', 'meals',
         'accommodation_local_cost', 'accommodation_local_currency', 'additional_expenses_initial', 'participation_local_cost', 'participation_local_currency', 'additional_expenses_initial_description', 'user_recommendation']
@@ -20,22 +20,22 @@ export const UIS = {
     layoutMap: {"reason": 100},
     fieldsets: [
       {
-        'label': 'User data',
+        'label': 'user_data.label',
         'fields': ['first_name', 'last_name', 'specialty', 'kind', 'tax_reg_num', 'tax_office', 'iban', 'user_category']
       },
        {
-        'label': 'Travel Data',
+        'label': 'travel_data.label',
         'fields': ['dse', 'project', 'reason', 'departure_point', 'arrival_point','movement_category', 'country_category',
         'task_start_date', 'task_end_date', 'depart_date', 'return_date', 'means_of_transport', 'transportation_cost', 'meals',
         'accommodation_local_cost', 'participation_local_cost', 'additional_expenses_initial', 'user_recommendation']
       },
       {
-        'label': 'Secretary Data',
+        'label': 'secretary_data.label',
         'fields': ['movement_num', 'expenditure_date_protocol',
         'expenditure_protocol', 'movement_date_protocol', 'movement_protocol', 'trip_days_before', 'non_grnet_quota',]
       },
       {
-        'label': 'Computed Data',
+        'label': 'computed_data.label',
         'fields': ['transport_days_manual', 'transport_days_proposed', 'overnights_num_manual',
         'overnights_num_proposed', 'compensation_days_manual', 'compensation_days_proposed', 'same_day_return_task', 
         'trip_days_before', 'trip_days_after', 'overnights_sum_cost', 'compensation_level', 'compensation_final', 'total_cost']
