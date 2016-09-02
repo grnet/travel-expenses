a743d378ecfc6f02cd6ff8fbae45f96b9a186443
Kostas Vogias <vogias@grnet.gr>
2016-09-02 17:49:25 +0300

    Extend manage.py to support location loading from csv file

--- travelsBackend/dumpdata_script.sh
+++
@@ -1,1 +0,0 @@
-python manage.py dumpdata --indent 2 -e contenttypes -e sessions --natural-foreign --natural-primary -o texpenses/fixtures/dbdump.json
---
+++ travelsBackend/texpenses/management/commands/loadlocations.py
@@ -0,0 +1,38 @@
+import sys
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from texpenses.models import (City, Country)
+
+CURRENCY = settings.DEFAULT_CURRENCY
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+class Command(BaseCommand):
+    help = "Loads location info (Countries,Cities etc) from a .csv file"
+
+    def add_arguments(self, parser):
+        parser.add_argument('locations')
+
+    def handle(self, *args, **options):
+        location_file_path = options['locations']
+        with open(location_file_path) as countries_csv_file:
+            for country_record in countries_csv_file:
+                country_name, city_name, category_name = country_record.\
+                    split(';')
+
+                country_obj, country_created = Country.objects.\
+                    get_or_create(name=country_name,
+                                  category=category_name,
+                                  currency=CURRENCY)
+                if country_created:
+                    self.stdout.write("Country:{0} is created.".
+                                      format(country_name))
+
+                city_obj, city_created = City.objects.\
+                    get_or_create(name=city_name,
+                                  country=country_obj)
+                if city_created:
+                    self.stdout.write("\tCity:{0}, is created.".
+                                      format(city_name))
