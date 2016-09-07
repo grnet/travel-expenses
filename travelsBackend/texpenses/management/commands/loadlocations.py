82f549ff909f03064fe57fd20b23ae41732a7040
Kostas Vogias <vogias@grnet.gr>
2016-09-07 16:03:23 +0300

    Fix loadlocations script to support django v1.7

--- travelsBackend/texpenses/management/commands/loadlocations.py
+++ travelsBackend/texpenses/management/commands/loadlocations.py
@@ -1,5 +1,7 @@
 import sys
 from django.core.management.base import BaseCommand
+from optparse import make_option
+
 from django.conf import settings
 from texpenses.models import (City, Country)
 
@@ -11,17 +13,15 @@ sys.setdefaultencoding('utf8')
 
 class Command(BaseCommand):
     help = "Loads location info (Countries,Cities etc) from a .csv file"
-
-    def add_arguments(self, parser):
-        parser.add_argument('locations')
-        parser.add_argument(
-            '--delete',
-            action='store_true',
-            dest='delete',
-            default=False,
-            help="Delete all inserted locations prior to loading the data "
-            "from CSV",
-        )
+    args = '<locations>'
+    option_list = BaseCommand.option_list + (
+        make_option('--delete',
+                    action='store_true',
+                    dest='delete',
+                    default=False,
+                    help="Delete all inserted locations prior to loading the"
+                    " data from CSV"),
+    )
 
     def preprocess(self, input):
         return input.strip().split(',')
@@ -38,7 +38,7 @@ class Command(BaseCommand):
         return (obj, created)
 
     def handle(self, *args, **options):
-        location_file_path = options['locations']
+        location_file_path = args[0]
         with open(location_file_path) as countries_csv_file:
 
             if options['delete']:
