911cfcd9bec40d5ffd87ff4eb9d4399303d5daae
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-09-12 16:06:26 +0300

    Create models for compensation related endpoints (without valid urls)
    - Compensation user save
    - Compensation secretary save

--- travelsFront/app/models/user-compensation.js
+++ travelsFront/app/models/user-compensation.js
@@ -1,5 +1,16 @@
-import DS from 'ember-data';
+import {SecretaryPetition} from 'travels-front/lib/models/secretary-petition';
+import {validator, buildValidations} from 'ember-cp-validations';
+import {UIS} from 'travels-front/lib/form-uis';
+import {normalizePetition, serializePetition} from 'travels-front/lib/models/util';
 
-export default DS.Model.extend({
 
+export default Petition.extend(Validations, {
+  __api__: {
+    path: '',
+    normalize: normalizePetition, 
+    serialize: serializePetition
+  },
+  __ui__: {
+   'default': UIS['compensation_user'],
+  } 
 });
