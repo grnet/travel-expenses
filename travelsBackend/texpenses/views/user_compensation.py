adbb813482666b6aa741ddee4e6c42a2e9ef9b56
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-09-13 11:10:34 +0300

    Add custom methods for UserCompensation view
    These methods provide two actions one for the save and one for the
    submission of a petition for compensation.

--- travelsBackend/texpenses/views/user_compensation.py
+++ travelsBackend/texpenses/views/user_compensation.py
@@ -1,7 +1,39 @@
-from texpenses.models import UserCompensation
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from texpenses.models import UserCompensation, UserCompensationSubmission
+from texpenses.generators.serializers import generate
 
 
-EXPOSED_METHODS = ['get_queryset']
+EXPOSED_METHODS = ['get_queryset', 'save', 'submit', 'get_serializer_class',
+                   'procceed']
+
+
+SUBMISSION_SERIALIZER = generate(UserCompensationSubmission)
+
+
+@detail_route(methods=['post', 'get'])
+def save(self, request, pk=None):
+    return self.procceed(request, pk)
+
+
+@detail_route(methods=['post', 'get'])
+def submit(self, request, pk=None):
+    return self.procceed(request, pk)
+
+
+def procceed(self, request, pk=None):
+    instance = self.get_object()
+    serializer = self.get_serializer(instance, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    if request.data.keys():
+        serializer.proceed(instance)
+    headers = self.get_success_headers(serializer.data)
+    return Response(serializer.data, headers=headers)
+
+
+def get_serializer_class(self):
+    return SUBMISSION_SERIALIZER if self.action == 'submit'\
+        else self.serializer_class
 
 
 def get_queryset(self):
