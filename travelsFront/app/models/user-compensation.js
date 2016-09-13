5c6851db383dcb83e4a0ee79f705d41d458a4df2
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-09-13 11:14:01 +0300

    Fix some errors

--- travelsFront/app/models/user-compensation.js
+++ travelsFront/app/models/user-compensation.js
@@ -1,10 +1,10 @@
-import {SecretaryPetition} from 'travels-front/lib/models/secretary-petition';
+import {Compensation} from 'travels-front/lib/models/compensation';
 import {validator, buildValidations} from 'ember-cp-validations';
 import {UIS} from 'travels-front/lib/form-uis';
 import {normalizePetition, serializePetition} from 'travels-front/lib/models/util';
 
 
-export default Petition.extend(Validations, {
+export default Compensation.extend({
   __api__: {
     path: '',
     normalize: normalizePetition, 
