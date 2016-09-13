bcc33df45241c7fd2fc235dc23fe5ca992caf18a
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-09-13 12:06:39 +0300

    Fix model imports and exports

--- travelsFront/app/models/secretary-compensation.js
+++ travelsFront/app/models/secretary-compensation.js
@@ -1,10 +1,10 @@
-import {Compensation} from 'travels-front/lib/models/compensation';
+import {SecretaryCompensation} from 'travels-front/lib/models/secretary-compensation';
 import {validator, buildValidations} from 'ember-cp-validations';
 import {UIS} from 'travels-front/lib/form-uis';
 import {normalizePetition, serializePetition} from 'travels-front/lib/models/util';
 
 
-export default Petition.extend(Validations, {
+export default SecretaryCompensation.extend(Validations, {
   __api__: {
     path: '',
     normalize: normalizePetition, 
