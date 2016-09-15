d16e42d714f0ac6b15236c9cf1e2bd31a7c5bee0
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-09-15 14:24:00 +0300

    Remove `get_serializer_class` method from corresponding view

--- travelsBackend/texpenses/views/user_compensation.py
+++ travelsBackend/texpenses/views/user_compensation.py
@@ -1,14 +1,9 @@
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-from texpenses.models import UserCompensation, UserCompensationSubmission
 from texpenses.generators.serializers import generate
 from texpenses.views import utils
 
-EXPOSED_METHODS = ['get_queryset', 'save', 'submit', 'get_serializer_class',
-                   'proceed']
-
-
-SUBMISSION_SERIALIZER = generate(UserCompensationSubmission)
+EXPOSED_METHODS = ['get_queryset', 'save', 'submit', 'proceed']
 
 
 @detail_route(methods=['post'])
@@ -19,12 +14,3 @@ def save(self, request, pk=None):
 @detail_route(methods=['post'])
 def submit(self, request, pk=None):
     return utils.proceed(self, request, pk)
-
-
-def get_serializer_class(self):
-    return SUBMISSION_SERIALIZER if self.action == 'submit'\
-        else self.serializer_class
-
-
-def get_queryset(self):
-    return UserCompensation.objects.filter(user=self.request.user)
