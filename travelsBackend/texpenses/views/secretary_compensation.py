c7b38d7cfac9de57f4a5b152824a782886678e08
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-09-15 15:40:12 +0300

    Add view actions for secretary compensation

--- travelsBackend/texpenses/views/secretary_compensation.py
+++ travelsBackend/texpenses/views/secretary_compensation.py
@@ -1,8 +1,23 @@
-from texpenses.models import SecretaryCompensation
+from rest_framework import status
+from rest_framework.decorators import detail_route
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from texpenses.models import Petition
+from texpenses.views import utils
+
+EXPOSED_METHODS = ['submit', 'save']
 
 
-EXPOSED_METHODS = ['get_queryset']
+@detail_route(methods=['post'])
+def save(self, request, pk=None):
+    return utils.proceed(self, request, pk)
 
 
-def get_queryset(self):
-    return SecretaryCompensation.objects.all()
+@detail_route(methods=['post'])
+def submit(self, request, pk=None):
+    instance = self.get_object()
+    petition_id = instance.proceed_next_status(
+        Petition.SECRETARY_COMPENSATION_SUBMISSION, delete=True)
+    headers = {'location': reverse('secretarycompensation-detail',
+               args=[petition_id])}
+    return Response(status=status.HTTP_303_SEE_OTHER, headers=headers)
