00e9864b1ac64713fd9c678b1c9948be6bc73773
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-10-24 17:58:58 +0300

    Create ajax call for password change action

---
+++ travelsFront/app/controllers/password.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import ENV from 'travels-front/config/environment';
+
+export default Ember.Controller.extend({
+		session: Ember.inject.service('session'),
+
+	actions: {
+
+		 changePassword() {
+      
+  		var token = this.get('session.data.authenticated.auth_token');
+  		var current_password = this.get('model.current_password');
+  		var new_password = this.get('model.new_password');
+  		var re_new_password = this.get('model.re_new_password');
+  
+	    return $.ajax({
+	    	method: "POST",
+	      headers:{
+	        Authorization: 'Token ' + token
+	      },
+	      url: ENV.APP.backend_host+"/auth/password/",
+	      data: {current_password: current_password, new_password: new_password, re_new_password: re_new_password},
+	    }).then(() => {
+	    	this.transitionToRoute('profile');
+	    });
+    },
+	}
+});
