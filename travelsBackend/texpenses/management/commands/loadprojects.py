4561d2c6b8e2696f26a114616fa992ed9d695c08
Kostas Vogias <vogias@grnet.gr>
2016-10-24 13:56:13 +0300

    Remove delete option from project loading mechanism

--- travelsBackend/texpenses/management/commands/loadprojects.py
+++ travelsBackend/texpenses/management/commands/loadprojects.py
@@ -11,14 +11,6 @@ sys.setdefaultencoding('utf8')
 class Command(BaseCommand):
     help = "Loads project info from a .csv file"
     args = '<projects>'
-    option_list = BaseCommand.option_list + (
-        make_option('--delete',
-                    action='store_true',
-                    dest='delete',
-                    default=False,
-                    help="Delete all inserted projects prior to loading the"
-                    " data from CSV"),
-    )
 
     def preprocess(self, input):
         data = input.strip().split(',')
@@ -42,14 +34,6 @@ class Command(BaseCommand):
         location_file_path = args[0]
         with open(location_file_path) as projects_csv_file:
 
-            load_in_bulk = False
-            bulk_data = []
-
-            if options['delete']:
-                self.stdout.write("Data insertion in bulk is activated...")
-                load_in_bulk = True
-                Project.objects.all().delete()
-
             for project_record in projects_csv_file:
                 name, accounting_code, manager_surname, manager_name,\
                     manager_email = self.preprocess(project_record)
@@ -60,19 +44,9 @@ class Command(BaseCommand):
                                 'manager_name': manager_name,
                                 'manager_email': manager_email
                                 }
-                if not load_in_bulk:
-                    project_obj, project_created = self.\
-                        get_or_create_extended(Project, **project_data)
+                project_obj, project_created = self.\
+                    get_or_create_extended(Project, **project_data)
 
-                    if project_created:
-                        self.stdout.write("Project:{0} is created.".
-                                          format(name))
-                else:
-                    self.stdout.write("Appending new project:{0}"
-                                      "to project list.".
+                if project_created:
+                    self.stdout.write("Project:{0} is created.".
                                       format(name))
-                    bulk_data.append(Project(**project_data))
-
-            if load_in_bulk:
-                self.stdout.write("Inserting data in bulk.")
-                Project.objects.bulk_create(bulk_data)
