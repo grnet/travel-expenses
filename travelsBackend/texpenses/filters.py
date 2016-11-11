1b9dad18204f44af5a492799869fa777e88929ad
Kostas Vogias <vogias@grnet.gr>
2016-11-11 16:23:33 +0200

    Implement a base filter

--- travelsBackend/texpenses/filters.py
+++ travelsBackend/texpenses/filters.py
@@ -4,13 +4,33 @@ from texpenses.models import Petition
 
 class PetitionFilter(filters.FilterSet):
 
-    created = filters.DateTimeFilter(name='created',
-                                     lookup_type='exact')
-    created_year__gt = filters.NumberFilter(name='created',
-                                            lookup_type='year__gt')
-    created_year__lt = filters.NumberFilter(name='created',
-                                            lookup_type='year__lt')
+    depart_date = filters.DateTimeFilter(name='travel_info__depart_date',
+                                         lookup_type='exact')
+    depart_date__gt = filters.DateTimeFilter(name='travel_info__depart_date',
+                                             lookup_type='gt')
+    depart_date__gte = filters.DateTimeFilter(name='travel_info__depart_date',
+                                              lookup_type='gte')
+    depart_date__lt = filters.DateTimeFilter(name='travel_info__depart_date',
+                                             lookup_type='lt')
+    depart_date__lte = filters.DateTimeFilter(name='travel_info__depart_date',
+                                              lookup_type='lte')
+
+    return_date = filters.DateTimeFilter(name='travel_info__return_date',
+                                         lookup_type='exact')
+    return_date__gt = filters.DateTimeFilter(name='travel_info__return_date',
+                                             lookup_type='gt')
+    return_date__gte = filters.DateTimeFilter(name='travel_info__return_date',
+                                              lookup_type='gte')
+    return_date__lt = filters.DateTimeFilter(name='travel_info__return_date',
+                                             lookup_type='lt')
+    return_date__lte = filters.DateTimeFilter(name='travel_info__return_date',
+                                              lookup_type='lte')
 
     class Meta:
         model = Petition
-        fields = ['created', 'created_year__gt', 'created_year__lt', 'project']
+        fields = ['id', 'first_name', 'last_name', 'created', 'updated',
+                  'depart_date', 'depart_date__gt', 'depart_date__gte',
+                  'depart_date__lt', 'depart_date__lte',  'project',
+                  'return_date', 'return_date__gt', 'return_date__lt',
+                  'return_date__gte', 'return_date__lte'
+                  ]
