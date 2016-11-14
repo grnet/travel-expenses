fa30797de581b7863f089fa9b7738240d4d75a6e
Katerina Sarlou <ksarlou@callc.grnet.gr>
2016-11-14 14:37:19 +0200

    Extend date-time de/serializer of DateTransform class to send local datetime values

--- travelsFront/app/app.js
+++ travelsFront/app/app.js
@@ -1,11 +1,39 @@
 import Ember from 'ember';
 import Resolver from './resolver';
+import DS from 'ember-data';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
+
+export default DS.DateTransform.reopen({
+
+	deserialize(serialized) {
+    var type = typeof serialized;
+
+    if (type === "string") {
+      return new Date(moment(serialized).format("YYYY-MM-DDTHH:mmZ"));
+    } else if (type === "number") {
+      return new Date(serialized);
+    } else if (serialized === null || serialized === undefined) {
+      // if the value is null return null
+      // if the value is not present in the data return undefined
+      return serialized;
+    } else {
+      return null;
+    }
+	},
+
+	serialize(date) {
+		if (date instanceof Date && !isNaN(date)) {
+      return moment(date).format("YYYY-MM-DDTHH:mm");
+    } else {
+      return null;
+		}
+	}
+});
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
