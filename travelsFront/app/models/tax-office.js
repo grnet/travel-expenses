ae9bfb9fd68fac7c7bf15ec168b07ed149eee2d8
Katerina Sarlou <ksarlou@admin.grnet.gr>
2016-12-09 17:18:17 +0200

    Create model tax-office

---
+++ travelsFront/app/models/tax-office.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  __api__: {
+    ns: 'resources'
+  },
+	name:  DS.attr(),
+	description:  DS.attr(),
+	address:  DS.attr(),
+	email:  DS.attr(),
+	phone:  DS.attr(),
+	url: DS.attr()
+});
