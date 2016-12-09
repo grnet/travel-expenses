766689b0284473dc364358206af6ce91a7526ecb
Katerina Sarlou <ksarlou@admin.grnet.gr>
2016-12-09 12:53:07 +0200

    Create auth gen

--- travelsFront/app/gen/auth.js
+++ travelsFront/app/gen/auth.js
@@ -1,9 +1,27 @@
+import gen from 'ember-gen/lib/gen';
 import AuthGen from 'ember-gen/lib/auth';
+import routes from 'ember-gen/lib/routes';
+
+const {
+  get, computed
+} = Ember;
 
 export default AuthGen.extend({
   login: {
     config: {
       authenticator: 'token'
     }
+  },
+
+  profile: {
+  	
+    modelName: 'profile',
+    menu: { display: true, icon: 'perm_identity',},
+
+    getModel() {
+      return get(this, 'store').findRecord('profile', 'me');
+    }
+  	
   }
 })
+
