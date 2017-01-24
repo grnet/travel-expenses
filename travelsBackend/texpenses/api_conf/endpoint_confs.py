09a9605daf133b2e6887280ff4d7038936cd09e4
Kostas Vogias <vogias@grnet.gr>
2017-01-24 15:12:58 +0200

    Define custom drf texpenses pagination class to spec

--- travelsBackend/texpenses/api_conf/endpoint_confs.py
+++ travelsBackend/texpenses/api_conf/endpoint_confs.py
@@ -2,6 +2,8 @@ from apimas.modeling.core import documents as doc
 from texpenses.models import Petition
 from rest_framework import serializers
 from texpenses.validators import required_validator
+from texpenses.pagination import TexpensesPagination
+from texpenses.filters import PetitionFilter
 import functools
 
 
@@ -20,6 +22,12 @@ class Configuration(object):
                     [functools.partial(required_validator,
                                        fields=Petition.USER_FIELDS)])
 
+        doc.doc_set(endpoint, ['.drf_collection', 'pagination_class'],
+                    TexpensesPagination)
+
+        doc.doc_set(endpoint, ['.drf_collection', 'filter_class'],
+                    PetitionFilter)
+
     def UserPetitionSubmitConfig(self):
         endpoint = self.spec['api']['petition/user/submitted']
         doc.doc_set(endpoint, ['*', 'status', '.drf_field', 'default'],
@@ -29,6 +37,12 @@ class Configuration(object):
         doc.doc_set(endpoint, ['*', 'user', '.drf_field', 'validators'],
                     [functools.partial(required_validator,
                                        fields=Petition.USER_FIELDS)])
+
+        doc.doc_set(endpoint, ['.drf_collection', 'pagination_class'],
+                    TexpensesPagination)
+
+        doc.doc_set(endpoint, ['.drf_collection', 'filter_class'],
+                    PetitionFilter)
 
     def SecretaryPetitionSaveConfig(self):
 
@@ -41,6 +55,12 @@ class Configuration(object):
         doc.doc_set(endpoint, ['*', 'status', '.drf_field', 'default'],
                     Petition.SAVED_BY_SECRETARY)
 
+        doc.doc_set(endpoint, ['.drf_collection', 'pagination_class'],
+                    TexpensesPagination)
+
+        doc.doc_set(endpoint, ['.drf_collection', 'filter_class'],
+                    PetitionFilter)
+
     def SecretaryPetitionSubmitConfig(self):
 
         endpoint = self.spec['api']['petition/secretary/submitted']
@@ -52,6 +72,12 @@ class Configuration(object):
         doc.doc_set(endpoint, ['*', 'status', '.drf_field', 'default'],
                     Petition.SUBMITTED_BY_SECRETARY)
 
+        doc.doc_set(endpoint, ['.drf_collection', 'pagination_class'],
+                    TexpensesPagination)
+
+        doc.doc_set(endpoint, ['.drf_collection', 'filter_class'],
+                    PetitionFilter)
+
     def UserCompensationConfig(self):
 
         endpoint = self.spec['api']['petition/user/compensations']
@@ -62,12 +88,24 @@ class Configuration(object):
         doc.doc_set(endpoint, ['*', 'status', '.drf_field', 'default'],
                     Petition.USER_COMPENSATION)
 
+        doc.doc_set(endpoint, ['.drf_collection', 'pagination_class'],
+                    TexpensesPagination)
+
+        doc.doc_set(endpoint, ['.drf_collection', 'filter_class'],
+                    PetitionFilter)
+
     def SecretaryCompensationConfig(self):
 
         endpoint = self.spec['api']['petition/secretary/compensations']
 
         doc.doc_set(endpoint, ['*', 'status', '.drf_field', 'default'],
                     Petition.SECRETARY_COMPENSATION)
+
+        doc.doc_set(endpoint, ['.drf_collection', 'pagination_class'],
+                    TexpensesPagination)
+
+        doc.doc_set(endpoint, ['.drf_collection', 'filter_class'],
+                    PetitionFilter)
 
     def configure_spec(self):
         self.UserPetitionConfig()
