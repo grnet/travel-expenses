2ad2def4cd5fe0295bd8707b7db12da09ae9a486
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-01-25 16:11:41 +0200

    Create pagination mixin

---
+++ travelsFront/tests/unit/mixins/pagination-test.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import PaginationMixin from 'travels-front/mixins/pagination';
+import { module, test } from 'qunit';
+
+module('Unit | Mixin | pagination');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let PaginationObject = Ember.Object.extend(PaginationMixin);
+  let subject = PaginationObject.create();
+  assert.ok(subject);
+});
