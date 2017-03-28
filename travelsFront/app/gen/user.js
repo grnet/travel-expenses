441589aa6f14563cd85202badaf32ca0471be398
Kostas Papadimitriou <kpap@grnet.gr>
2017-03-28 19:43:49 +0300

    ui: Set resource name and menu display attrs in gen/user

--- travelsFront/app/gen/user.js
+++ travelsFront/app/gen/user.js
@@ -1,7 +1,15 @@
 import gen from 'ember-gen/lib/gen';
 
 export default gen.CRUDGen.extend({
+  // key used in apimas spec
+  resourceName: 'users',
+  // related ember-data model
   modelName: 'user',
+
+  // require authenticated user
   auth: true,
+  menu: {
+    display: true
+  }
 });
 
