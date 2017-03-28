9a7214e7055099c855bbe58d0390635b9e018b64
Kostas Papadimitriou <kpap@grnet.gr>
2017-03-28 20:06:34 +0300

    ui: gen/model for `project`

---
+++ travelsFront/app/gen/project.js
@@ -0,0 +1,9 @@
+import gen from 'ember-gen/lib/gen';
+
+export default gen.CRUDGen.extend({
+  modelName: 'project',
+  resourceName: 'project',
+  menu: {
+    display: true
+  }
+});
---
+++ travelsFront/app/models/project.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  __api__: {
+    path: 'project' // ember-data would use `project*s*`
+  },
+  name: DS.attr(),
+  accounting_code: DS.attr(),
+  manager_name: DS.attr(),
+  manager_surname: DS.attr(),
+  manager_email: DS.attr()
+});
