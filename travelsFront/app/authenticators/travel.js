d75b29e1675b5d3da3face6f8be627160f11f124
Kostas Papadimitriou <kpap@grnet.gr>
2017-03-30 14:51:53 +0300

    ui: Use detailed profile endpoint to resolve authenticated user attrs

--- travelsFront/app/authenticators/travel.js
+++ travelsFront/app/authenticators/travel.js
@@ -1,6 +1,7 @@
 import ApimasAuthenticator from 'ember-gen-apimas/authenticators/apimas';
 
 export default ApimasAuthenticator.extend({
+  profilePath: '/auth/me/detailed',
   processProfileData(user) {
     if (!user.role) { user.role = 'USER'; }
     return user;
