48d826b1e9bf8dc2d817ca278881b6a21156839e
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-04-07 14:50:05 +0300

    Remove unnecessary properties from project model

--- travelsFront/app/models/project.js
+++ travelsFront/app/models/project.js
@@ -4,13 +4,9 @@ export default DS.Model.extend({
   __api__: {
     ns: '/'
   },
-  full_label: Ember.computed('name', function(){
-    return this.get('name') + " (" + this.get('manager_name') + " " + this.get('manager_surname') + ")"
-  }),
+
 	name: DS.attr(),
 	accountingCode: DS.attr(),
-	manager_name: DS.attr(),
-	manager_surname: DS.attr(),
 	manager: DS.attr(),
 	url: DS.attr()
 });
