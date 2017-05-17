bcc8ec528e56613d6277d8b11f1a18f2bae08473
Kostas Vogias <vogias@grnet.gr>
2017-05-17 15:18:06 +0300

    Create new migrations after rebase

---
+++ travelsBackend/texpenses/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('texpenses', '0007_auto_20170407_1638'),
+        ('texpenses', '0008_city_timezone'),
+    ]
+
+    operations = [
+    ]
