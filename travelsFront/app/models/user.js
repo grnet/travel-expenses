765a1a4a26829d0e1fbbd44f74767b1b4992d0b6
Katerina Sarlou <ksarlou@admin.grnet.gr>
2017-10-05 12:20:02 +0300

    Update user model

--- travelsFront/app/models/user.js
+++ travelsFront/app/models/user.js
@@ -1,6 +1,15 @@
 import DS from 'ember-data';
 
+const SIGNUP_UI = {}
+
 export default DS.Model.extend({
+  __api__: {
+    ns: 'auth',
+    path: 'register'
+  },
+  __ui__: {
+    'signup': SIGNUP_UI,
+  },
   username: DS.attr('string'),
   password: DS.attr('string', {formAttrs: {type: 'password'}}),
   email: DS.attr('string')
