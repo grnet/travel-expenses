06a875075c03cefcc274216925ac30d4fd47a234
Kostas Vogias <vogias@grnet.gr>
2017-10-25 14:35:18 +0300

    Customize city admin

--- travelsBackend/texpenses/admin.py
+++ travelsBackend/texpenses/admin.py
@@ -16,14 +16,18 @@ class PetitionAdmin(admin.ModelAdmin):
     list_filter = ('created', 'updated', 'task_start_date', 'task_end_date',
                    'project', 'user')
     date_hierarchy = 'updated'
-    ordering = ('-updated','-task_start_date', '-task_end_date')
+    ordering = ('-updated', '-task_start_date', '-task_end_date')
 
+
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_filter = ('country',)
 
 admin.site.register(UserProfile)
 admin.site.register(TaxOffice)
 admin.site.register(Petition, PetitionAdmin)
 admin.site.register(Project)
 admin.site.register(Country)
-admin.site.register(City)
+admin.site.register(City, CityAdmin)
 admin.site.register(TravelInfo)
 admin.site.register(CityDistances)
