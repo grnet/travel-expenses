0f3535b4a2e8f113d8b23bb984e78f57ceef414c
Kostas Vogias <vogias@grnet.gr>
2017-12-01 15:10:35 +0200

    Dont email withdrawn petitions

--- travelsBackend/texpenses/actions.py
+++ travelsBackend/texpenses/actions.py
@@ -145,11 +145,11 @@ def compensation_alert():
 
     inform_date = (timezone.now() - timedelta(days=1)).strftime(DATE_FORMAT)
 
-    approved_petitions = Petition.objects.filter(
-        status=Petition.APPROVED_BY_PRESIDENT,
-        deleted=False,
-        compensation_alert=False)
-
+    approved_petitions = Petition.objects.filter(status=\
+                                               Petition.APPROVED_BY_PRESIDENT,\
+						 deleted=False,
+                                                 compensation_alert=False,
+                                                 withdrawn=False)
     for petition in approved_petitions:
         return_date = petition.travel_info.last().return_date.\
             strftime(DATE_FORMAT)
