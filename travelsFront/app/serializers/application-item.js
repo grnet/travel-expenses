a9156319d7af6d840bd0645330de01f58239c5c4
Kostas Papadimitriou <kpap@grnet.gr>
2017-12-29 13:13:55 +0200

    ui: Indicate travel_info field as embedded

---
+++ travelsFront/app/serializers/application-item.js
@@ -0,0 +1,6 @@
+import Serializer from './application';
+import DS from 'ember-data';
+
+export default Serializer.extend(DS.EmbeddedRecordsMixin, {
+  attrs: { 'travel_info': { embedded: 'always' } }
+})
