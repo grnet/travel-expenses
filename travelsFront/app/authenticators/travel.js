38546fbdd736ce91aab1d266899528735aa11066
Olga Brani <olgabrani78@gmail.com>
2018-03-06 12:15:25 +0200

    Return role after user login
    Role will be used for applying permission rules

--- travelsFront/app/authenticators/travel.js
+++ travelsFront/app/authenticators/travel.js
@@ -8,6 +8,10 @@ const {
 export default ApimasAuthenticator.extend({
   profilePath: '/auth/me/detailed',
   processProfileData(data, profile) {
+    data['role'] = 'anonymous';
+    if (data.hasOwnProperty('user_group')) {
+      data['role'] = data.user_group;
+    }
     merge(data, profile);
     return data;
   }
