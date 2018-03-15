b8c26fb7876308a863636d0b3698056b6f072953
Efthymia Bika <ey8ymia@grnet.gr>
2018-03-15 10:52:42 +0200

    Merge migrations

---
+++ travelsBackend/texpenses/migrations/0032_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('texpenses', '0027_merge'),
+        ('texpenses', '0031_auto_20180226_0946'),
+    ]
+
+    operations = [
+    ]
