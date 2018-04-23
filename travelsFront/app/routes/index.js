6ea2f98ea55ee137fa26c5ddd31ba6e7f45bf213
Katerina Sarlou <ksarlou@admin.grnet.gr>
2018-04-23 13:06:50 +0300

    Implement login redirect functionality
    The default login redirectroute is the application list route
    but if the profile elements are empty the login redirect route
    is the profile route.

--- travelsFront/app/routes/index.js
+++ travelsFront/app/routes/index.js
@@ -1,10 +1,30 @@
 import Ember from 'ember';
 
 const DEFAULT_ROUTE = 'application-item';
+const PROFILE_ROUTE = '/auth/my-profile';
+const PROFILE_PARAMS = ['first_name', 'last_name', 'iban', 'specialty', 'kind', 'tax_office', 'tax_reg_num'];
+
+function profileIsFilled(session) {
+  let profile = session.get('session.authenticated');
+
+  for (let item of PROFILE_PARAMS) {
+    if (profile[item] == null) {
+      return false;
+    }
+  }
+  return true;
+};
 
 export default Ember.Route.extend({
   session: Ember.inject.service(),
+
   beforeModel(transition) {
-    this.transitionTo(DEFAULT_ROUTE);
+    let session = this.get('session');
+
+    if (profileIsFilled(session)) {
+      this.transitionTo(DEFAULT_ROUTE);
+    } else {
+      this.transitionTo(PROFILE_ROUTE);
+    };
   },
 });
