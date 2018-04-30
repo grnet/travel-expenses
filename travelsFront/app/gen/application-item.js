101af70336f051abe764262b6c0f624c2c2582fc
Katerina Sarlou <ksarlou@admin.grnet.gr>
2018-04-30 17:31:18 +0300

    Refactor code concerning ability states

--- travelsFront/app/gen/application-item.js
+++ travelsFront/app/gen/application-item.js
@@ -6,6 +6,7 @@ import { applicationActions } from '../utils/common/actions';
 import USER from '../utils/application/user';
 import SECRETARY from '../utils/application/secretary';
 import CONTROLLER from '../utils/application/controller';
+import {abilityStates} from 'travel/lib/application/abilityStates';
 
 const {
   get,
@@ -15,18 +16,6 @@ const {
 
 const CHOICES = ENV.APP.resources;
 
-const STATUS_MAP = {
-  'SAVED_BY_USER': 1,
-  'SUBMITTED_BY_USER': 2,
-  'SAVED_BY_SECRETARY': 3,
-  'SUBMITTED_BY_SECRETARY': 4,
-  'APPROVED_BY_PRESIDENT': 5,
-  'USER_COMPENSATION': 6,
-  'USER_COMPENSATION_SUBMISSION': 7,
-  'SECRETARY_COMPENSATION': 8,
-  'SECRETARY_COMPENSATION_SUBMISSION': 9,
-  'PETITION_FINAL_APPOVAL': 10,
-}
 
 export default gen.CRUDGen.extend({
   order: 200,
@@ -39,58 +28,7 @@ export default gen.CRUDGen.extend({
     role: reads('session.session.authenticated.user_group'),
   },
 
-  abilityStates: {
-    usersaved: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['SAVED_BY_USER'];
-    }),
-    usersubmitted: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['SUBMITTED_BY_USER'];
-    }),
-    secretarysaved: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['SAVED_BY_SECRETARY'];
-    }),
-    secretarysubmitted: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['SUBMITTED_BY_SECRETARY'];
-    }),
-    presidentapproved: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['APPROVED_BY_PRESIDENT'];
-    }),
-    usercompensationsaved: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['USER_COMPENSATION'];
-    }),
-    usercompensationsubmitted: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['USER_COMPENSATION_SUBMISSION'];
-    }),
-    secretarycompensationsaved: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['SECRETARY_COMPENSATION'];
-    }),
-    secretarycompensationsubmitted: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['SECRETARY_COMPENSATION_SUBMISSION'];
-    }),
-    presidentcompensationapproved: computed('model.status', function() {
-      let status = this.get('model.status');
-
-      return status === STATUS_MAP['PETITION_FINAL_APPOVAL'];
-    }),
-  },
+  abilityStates: abilityStates,
 
   common: {
     preloadModels: ['city'],
