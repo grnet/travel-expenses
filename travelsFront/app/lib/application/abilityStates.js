a4d847558fca22396c33bfcf21bd9dae363e6147
Katerina Sarlou <ksarlou@admin.grnet.gr>
2018-06-26 16:28:32 +0300

    Add methods in the abilityStates that define the owner of the application

--- travelsFront/app/lib/application/abilityStates.js
+++ travelsFront/app/lib/application/abilityStates.js
@@ -22,10 +22,23 @@ let abilityStates = {
 
     return status === STATUS_MAP['SAVED_BY_USER'];
   }),
+  ownedusersaved: computed('model.status', 'model.user_id', function() {
+    let status = this.get('model.status');
+    let userId = this.get('session.session.authenticated.id');
+    let applicationUserId = this.get('model.user_id');
+    return userId === applicationUserId && status === STATUS_MAP['SAVED_BY_USER'];
+  }),
   usersubmitted: computed('model.status', function() {
     let status = this.get('model.status');
 
     return status === STATUS_MAP['SUBMITTED_BY_USER'];
+  }),
+  ownedusersubmitted: computed('model.status', 'model.user_id', function() {
+    let status = this.get('model.status');
+    let userId = this.get('session.session.authenticated.id');
+    let applicationUserId = this.get('model.user_id');
+
+    return userId === applicationUserId && status === STATUS_MAP['SUBMITTED_BY_USER'];
   }),
   secretarysaved: computed('model.status', function() {
     let status = this.get('model.status');
@@ -42,15 +55,36 @@ let abilityStates = {
 
     return status === STATUS_MAP['APPROVED_BY_PRESIDENT'];
   }),
+  ownedpresidentapproved: computed('model.status', 'model.user_id', function() {
+    let status = this.get('model.status');
+    let userId = this.get('session.session.authenticated.id');
+    let applicationUserId = this.get('model.user_id');
+
+    return userId === applicationUserId && status === STATUS_MAP['APPROVED_BY_PRESIDENT'];
+  }),
   usercompensationsaved: computed('model.status', function() {
     let status = this.get('model.status');
 
     return status === STATUS_MAP['USER_COMPENSATION'];
   }),
+  ownedusercompensationsaved: computed('model.status', 'model.user_id', function() {
+    let status = this.get('model.status');
+    let userId = this.get('session.session.authenticated.id');
+    let applicationUserId = this.get('model.user_id');
+
+    return userId === applicationUserId && status === STATUS_MAP['USER_COMPENSATION'];
+  }),
   usercompensationsubmitted: computed('model.status', function() {
     let status = this.get('model.status');
 
     return status === STATUS_MAP['USER_COMPENSATION_SUBMISSION'];
+  }),
+  ownedusercompensationsubmitted: computed('model.status', 'model.user_id', function() {
+    let status = this.get('model.status');
+    let userId = this.get('session.session.authenticated.id');
+    let applicationUserId = this.get('model.user_id');
+
+    return userId === applicationUserId && status === STATUS_MAP['USER_COMPENSATION_SUBMISSION'];
   }),
   secretarycompensationsaved: computed('model.status', function() {
     let status = this.get('model.status');
