37d295108b3796fbbddab075a100c4df965b5921
Efthymia Bika <ey8ymia@grnet.gr>
2018-07-18 13:04:06 +0300

    Set trip_days_left when proceeding in status 7

--- travelsBackend/texpenses/views/mixins.py
+++ travelsBackend/texpenses/views/mixins.py
@@ -234,7 +234,7 @@ class ApplicationMixin(object):
         application_id = application.proceed()
         application_status = application.status
 
-        if application_status == Petition.USER_COMPENSATION:
+        if application_status == Petition.USER_COMPENSATION_SUBMISSION:
             application.set_trip_days_left()
 
         per_status_email_confs = {
