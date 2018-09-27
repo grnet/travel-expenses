00efba46b09f0fc0248ccbca53ad1890ea404197
Antonis Angelakis <angelakis@grnet.gr>
2018-09-27 14:09:20 +0300

    Make petition filters return distinct results

--- travelsBackend/texpenses/filters.py
+++ travelsBackend/texpenses/filters.py
@@ -5,31 +5,31 @@ from texpenses.models import Petition
 class PetitionFilter(filters.FilterSet):
 
     depart_date = filters.DateTimeFilter(name='travel_info__depart_date',
-                                         lookup_expr='exact')
+                                         distinct=True, lookup_expr='exact')
     depart_date__gt = filters.DateTimeFilter(name='travel_info__depart_date',
-                                             lookup_expr='gt')
+                                             distinct=True, lookup_expr='gt')
     depart_date__gte = filters.DateTimeFilter(name='travel_info__depart_date',
-                                              lookup_expr='gte')
+                                              distinct=True, lookup_expr='gte')
     depart_date__lt = filters.DateTimeFilter(name='travel_info__depart_date',
-                                             lookup_expr='lt')
+                                             distinct=True, lookup_expr='lt')
     depart_date__lte = filters.DateTimeFilter(name='travel_info__depart_date',
-                                              lookup_expr='lte')
+                                              distinct=True, lookup_expr='lte')
 
     return_date = filters.DateTimeFilter(name='travel_info__return_date',
-                                         lookup_expr='exact')
+                                         distinct=True, lookup_expr='exact')
     return_date__gt = filters.DateTimeFilter(name='travel_info__return_date',
-                                             lookup_expr='gt')
+                                             distinct=True, lookup_expr='gt')
     return_date__gte = filters.DateTimeFilter(name='travel_info__return_date',
-                                              lookup_expr='gte')
+                                              distinct=True, lookup_expr='gte')
     return_date__lt = filters.DateTimeFilter(name='travel_info__return_date',
-                                             lookup_expr='lt')
+                                             distinct=True, lookup_expr='lt')
     return_date__lte = filters.DateTimeFilter(name='travel_info__return_date',
-                                              lookup_expr='lte')
+                                              distinct=True, lookup_expr='lte')
 
     first_name = filters.CharFilter(name='first_name',
-                                    lookup_expr='unaccent__icontains')
+                                    distinct=True, lookup_expr='unaccent__icontains')
     last_name = filters.CharFilter(name='last_name',
-                                   lookup_expr='unaccent__icontains')
+                                   distinct=True, lookup_expr='unaccent__icontains')
 
     class Meta:
         model = Petition
