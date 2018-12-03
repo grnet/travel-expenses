4041cfbe202defac54bfc0cc09475576d1d6729b
Antonis Angelakis <angelakis@grnet.gr>
2018-12-03 18:53:29 +0200

    Merge migrations

---
+++ travelsBackend/texpenses/migrations/0040_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('texpenses', '0039_travelinfo_no_transportation_calculation'),
+        ('texpenses', '0039_petition_additional_expenses_grnet'),
+    ]
+
+    operations = [
+    ]
