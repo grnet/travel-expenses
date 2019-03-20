25368daf5b66b1f97952171d268cf7d2eb6e6ad3
Antonis Angelakis <angelakis@grnet.gr>
2019-03-20 12:21:48 +0200

    Fix failing test

--- travelsBackend/texpenses/tests/test_api_application_workflow.py
+++ travelsBackend/texpenses/tests/test_api_application_workflow.py
@@ -830,7 +830,7 @@ class TestApi(APITestCase):
 
         url_stats = reverse('api_project-stats')
         response = self.client.get(url_stats, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_project_api_as_controller(self):
         self._set_up()
