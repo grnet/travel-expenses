61854431093503132b12306b3fa9ada64c61f8ed
Antonis Angelakis <angelakis@grnet.gr>
2019-03-22 17:35:49 +0200

    Determine offset from actual time and add logging

--- travelsBackend/texpenses/migrations/0041_auto_20190201_1417.py
+++ travelsBackend/texpenses/migrations/0041_auto_20190201_1417.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
 import pytz
 from datetime import datetime
 from django.db import migrations
+
+logger = logging.getLogger(__name__)
 
 
 def convert_datetimes(apps, schema_editor):
@@ -11,6 +14,8 @@ def convert_datetimes(apps, schema_editor):
     bug_start_date = datetime(2018, 10, 4, tzinfo=pytz.UTC)
     for application in Petition.objects.filter(created__gt=bug_start_date):
         if not application.travel_info.exists():
+            logger.info('Application w/ DSE %s has no travel infos, skipping' %
+                        application.dse)
             continue
         fix_task_dates(application)
         for travel in application.travel_info.all():
@@ -20,37 +25,42 @@ def convert_datetimes(apps, schema_editor):
         application.save()
 
 
-def calculate_shift_needed(time_created, correct_tz):
+def shift_time(dt, correct_tz):
     base_tz = pytz.timezone('Europe/Athens')
-    base_offset = time_created.astimezone(base_tz).utcoffset()
-    correct_offset = time_created.astimezone(correct_tz).utcoffset()
-    return (base_offset - correct_offset)
+    base_offset = dt.astimezone(base_tz).utcoffset()
+    correct_offset = dt.astimezone(correct_tz).utcoffset()
+    shift_needed = base_offset - correct_offset
+    dt += shift_needed
+    return shift_needed
 
 
 def fix_task_dates(application):
     local_city = application.travel_info.first().arrival_point
     correct_tz = pytz.timezone(local_city.timezone)
-    shift_needed = calculate_shift_needed(application.created, correct_tz)
-    application.task_start_date += shift_needed
-    application.task_end_date += shift_needed
+    start_shifted = shift_time(application.task_start_date, correct_tz)
+    end_shifted = shift_time(application.task_end_date, correct_tz)
+    logger.info('Application %s, task_start_date shifted %s' %
+                (application.id, start_shifted))
+    logger.info('Application %s, task_end_date shifted %s' %
+                (application.id, end_shifted))
 
 
 def fix_depart_date(travel):
     if not travel.depart_date:
         return
     correct_tz = pytz.timezone(travel.departure_point.timezone)
-    time_created = travel.travel_petition.created
-    shift_needed = calculate_shift_needed(time_created, correct_tz)
-    travel.depart_date += shift_needed
+    shifted = shift_time(travel.depart_date, correct_tz)
+    logger.info('Travel %s, depart_date shifted %s' %
+                (travel.id, shifted))
 
 
 def fix_return_date(travel):
     if not travel.return_date:
         return
     correct_tz = pytz.timezone(travel.arrival_point.timezone)
-    time_created = travel.travel_petition.created
-    shift_needed = calculate_shift_needed(time_created, correct_tz)
-    travel.return_date += shift_needed
+    shifted = shift_time(travel.return_date, correct_tz)
+    logger.info('Travel %s, return_date shifted %s' %
+                (travel.id, shifted))
 
 
 class Migration(migrations.Migration):
